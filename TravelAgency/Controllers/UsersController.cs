eddd33d805f6649799f366207242355d1299f0d7
akikas01 <andrewsk1k96@gmail.com>
2025-06-19 09:11:46 +0300

    added

---
+++ TravelAgency/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelAgency.Models;
+
+namespace TravelAgency.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly TRAVEL_AGENCYContext _context;
+
+        public UsersController(TRAVEL_AGENCYContext context)
+        {
+            _context = context;
+        } 
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (UserExists(user.Username))
+                {
+                    return Conflict("Username Already Exists!");
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+
+            return CreatedAtAction("GetUser", new { id = user.Username }, user);
+        }
+
+        
+
+        private bool UserExists(string id)
+        {
+            return _context.Users.Any(e => e.Username == id);
+        }
+    }
+}
---
+++ TravelAgency/PasswordHasher.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+public static class PasswordHasher
+{
+    // Hash password with randomly generated salt
+    public static string HashPassword(string password)
+    {
+        // Generate a 128-bit salt using a secure PRNG
+        byte[] salt = new byte[16];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(salt);
+        }
+
+        // Derive a 256-bit subkey (32 bytes) using PBKDF2
+        byte[] hash = KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 32);
+
+        // Combine salt + hash (Base64) using ":" delimiter
+        return $"{Convert.ToBase64String(salt)}:{Convert.ToBase64String(hash)}";
+    }
+
+    // Verify password using stored hash
+    public static bool VerifyPassword(string password, string storedHash)
+    {
+        var parts = storedHash.Split(':');
+        if (parts.Length != 2) return false;
+
+        byte[] salt = Convert.FromBase64String(parts[0]);
+        byte[] storedHashBytes = Convert.FromBase64String(parts[1]);
+
+        // Hash input password with same salt
+        byte[] attemptedHash = KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 32);
+
+        return CryptographicOperations.FixedTimeEquals(storedHashBytes, attemptedHash);
+    }
+}
+
