d31615ae5006301faa5ee3612115be70911ae5f6
akikas01 <andrewsk1k96@gmail.com>
2025-06-23 11:42:05 +0300

    Completed Destinations controller

--- TravelAgency/Controllers/DestinationsController.cs
+++ TravelAgency/Controllers/DestinationsController.cs
@@ -9,7 +9,7 @@ namespace TravelAgency.Controllers
     [ApiController]
     public class DestinationsController : ControllerBase
     {
-        
+
         private readonly TRAVEL_AGENCYContext _context;
         public DestinationsController(TRAVEL_AGENCYContext context)
         {
@@ -22,7 +22,7 @@ namespace TravelAgency.Controllers
         {
             foreach (var destination in destinations)
             {
-                if (_context.Destinations.Any(e => e.Country==destination.Country && e.TravelPackage == destination.TravelPackage))
+                if (_context.Destinations.Any(e => e.Country == destination.Country && e.TravelPackage == destination.TravelPackage))
                 {
                     continue;//If the same record exists in the database it doesn't insert the same record, it skips it.
                 }
@@ -36,5 +36,54 @@ namespace TravelAgency.Controllers
 
             return Ok();
         }
+
+
+
+        [HttpGet("country/{Country}")]
+        public async Task<ActionResult<IEnumerable<String>>> GetTravelPackages(string Country)
+        {
+            List<Destination> destinations = _context.Destinations.Where(p => p.Country == Country).ToList();
+
+
+            if (destinations.Count == 0)
+            {
+
+                return NotFound("No Travel Packages includes this country!");
+
+            }
+            List<String> travelPackages = new List<String>();
+            foreach (Destination destination in destinations)
+            {
+
+                travelPackages.Add(destination.TravelPackage);
+            }
+
+            return Ok(travelPackages);
+        }
+
+        [HttpGet("TravelPackage/{title}")]
+        public async Task<ActionResult<IEnumerable<String>>> GetCountries(string title)
+        {
+            List<Destination> destinations = _context.Destinations.Where(p => p.TravelPackage == title).ToList();
+
+
+            if (destinations.Count == 0)
+            {
+
+                return NotFound("No Travel Package found in Destinations");
+
+            }
+            List<String> countries = new List<String>();
+            foreach (Destination destination in destinations)
+            {
+
+                countries.Add(destination.Country);
+            }
+
+            return Ok(countries);
+        }
+
+
+
     }
 }
